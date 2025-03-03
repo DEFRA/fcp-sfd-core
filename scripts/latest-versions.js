fcb777dbace9e42cc08c6d8bc3c39225f1b14681
jungabunga <amir.ishmuhametov@gmail.com>
2025-03-03 09:33:13 +0000

    removed artifacts and adjusted ports

--- scripts/latest-versions.js
+++ scripts/latest-versions.js
@@ -6,12 +6,10 @@ const gitBaseUrl = 'https://api.github.com/repos/DEFRA'
 const servicesPath = `${cwd}/service-compose`
 
 const getLatestRelease = async (repo) => {
-  console.log(`Checking ${repo}...`)
-  console.log(`${gitBaseUrl}/${repo}/tags`)
   const response = await fetch(`${gitBaseUrl}/${repo}/tags`)
   const data = await response.json()
 
-  const version = data.length > 0 ? data[0].name : 'No tags found'
+  const version = data[0]?.name ?? 'No tags found'
 
   console.log(`${repo}: ${version}`)
 }
